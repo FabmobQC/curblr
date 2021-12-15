523a4be330c174555f33305cd14b801c57d296ba
idillon-sfl <io.daza-dillon@savoirfairelinux.com>
2021-12-15 11:08:13 -0500

    Add required manifest field to curblr for mtl

--- Curblr/conversion-datas/curblr-datamtl-convert/scripts/segment_to_curblr.js
+++ Curblr/conversion-datas/curblr-datamtl-convert/scripts/segment_to_curblr.js
@@ -5,6 +5,10 @@ const jsonHelper = require("./json_helper");
 function convertToCurblr(rpaCode, input) {
   const geojson = {};
   geojson['manifest'] = {
+    "createdDate": (new Date()).toISOString(), // required
+    "curblrVersion": "1.1.0", // required
+    "timeZone": "America/Toronto", // required
+    "currency": "CAD", // required
     "priorityHierarchy": [
       "1",
       "2",
@@ -12,8 +16,11 @@ function convertToCurblr(rpaCode, input) {
       "4",
       "5",
       "free parking"
-    ],
-    "curblrVersion": "1.1.0",
+    ], // required
+    "authority": {
+      "name": "Fabrique des Mobilités Québec", // required
+      "url": "https://fabmobqc.ca/", // required
+    }
   }
   geojson['type'] = 'FeatureCollection';
   geojson['features'] = [];
@@ -28,6 +35,7 @@ function convertToCurblr(rpaCode, input) {
         pp_panneau_id_pan: derivedFrom
     } = feature.properties
     
+    // Why is this rounded??
     shstLocationStart = Math.round(shstLocationStart);
     shstLocationEnd = Math.round(shstLocationEnd);
 
@@ -65,4 +73,8 @@ if (typeof require !== 'undefined' && require.main === module) {
   const output = convertToCurblr(rpaCode, input);
   
   jsonHelper.write(outputFilename, output, false);
+}
+
+module.exports = {
+  convertToCurblr,
 }
